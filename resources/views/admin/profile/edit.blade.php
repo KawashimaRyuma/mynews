6ca08ffc2ce87d4c1106e549ff0831c21c82df9b
KawashimaRyuma <ryumatennis@icloud.com>
2021-08-15 06:14:29 +0000

    laravel

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+    <html>
+        <head>
+        <meta charset="ytf-8">
+        <meta http-equiv="X-UACompatible"
+content="IE=edge">
+        <meta name="viewport" content="width=device-width,
+initial-scale=1">
+
+        <title>MyProfile</title>
+    </head>
+    <body>
+        <h1>Myプロファイル</h1>
+　　</body>
+</html>
