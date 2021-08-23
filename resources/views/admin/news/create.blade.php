3ec97f018f0876f8f0d6af0931dabc3c73ec2f39
KawashimaRyuma <ryumatennis@icloud.com>
2021-08-23 03:33:44 +0000

    laravel 11

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,14 +1,15 @@
-<DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-epuiv="X-UA-Compatible"
-content="IE=edge">
-        <meta name="viewport" content="width=device-width,
-initial-scale=1">
+{{-- layouts/admin.blade.phpを読み込む　--}}
+@extends('layouts.admin')
 
-        <title>MyNews</title>
-    </head>
-        <h1>Myニュース作成画面</h1>
-    </body>
-</html>
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成を埋め込む--}}
+@section('title', 'プロフィール画面')
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む--}}
+@section('content')
+    <div class="container">
+        <div class="row">
+          <div class="col-md-8 mx-auto">
+              <h2>プロフィール</h2>
+          </div>
+        </div>
+    </div>
+@endsection
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -7,9 +7,22 @@ content="IE=edge">
         <meta name="viewport" content="width=device-width,
 initial-scale=1">
 
-        <title>MyProfile</title>
+        <title>profile</title>
     </head>
     <body>
-        <h1>Myプロファイル</h1>
-　　</body>
+        <h1>プロフィール画面</h1>
+        
+    @extends('layouts.profile')
+        @section('title', 'プロフィール')
+        
+        @section('content')
+    <div class="container">
+        <div class="row">
+            <div>
+              <h2>プロフィール</h2>
+          </div>
+        </div>
+    </div>
+@endsection
+</body>
 </html>
